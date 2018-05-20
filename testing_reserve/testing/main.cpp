f08be3e403dccd80bdfde36b1de6a15ccf01854d
Liudmyla <vohony1@gmail.com>
2018-05-20 16:35:20 +0300

    update testing reserve (project with money counter)

--- testing_reserve/testing/main.cpp
+++ testing_reserve/testing/main.cpp
@@ -1,112 +1,27 @@
-#include <string.h>
-#include <stdio.h>
 #include <iostream>
-#include <assert.h>
-#include "header.h"
-
+#include <stdio.h>
 using namespace std;
 
-void InitMindGame()
+const int SIZE = 8;
+int money[SIZE] = {1, 2, 5, 10, 20, 50, 100, 200};
+
+
+int N(int S, int i)
 {
-	int min_value = 0;
-	int max_value = 0;
-	cout<<"Enter min value:\n";
-	cin>>min_value;
-	while(cin.fail()) {
-		cout << "Error! Enter a number, please" << endl;
-		cin.clear();
-		cin.ignore(256,'\n');
-		cin >> min_value;
-	}
-	cout<<"Enter max value:\n";
-	cin>>max_value;
-	while(cin.fail() || max_value <= min_value)
+	if (i == 1000)
 	{
-		if (cin.fail() )
-			cout << "Error! Enter a number, please" << endl;
-		else
-			cout << "Error! Enter a number, bigger then min value" << endl;
-		cin.clear();
-		cin.ignore(256,'\n');
-		cin >> max_value;
+		i = SIZE-1;
 	}
-	StartGame(min_value, max_value);
+	if (S < 0 || i < 0) return 0;
+	if (S == 0 || i == 0) return 1;
+	return N(S, i - 1) + N(S - money[i], i);
 }
 
-int TryFoundNumber(int num)
-{
-	cin>>num;
-	while(cin.fail()) {
-		cout << "It's not a number. Try again\n" << endl;
-		cin.clear();
-		cin.ignore(256,'\n');
-		cin >> num;
-	}
-	return num;
-}
+int main(){
+	setlocale (LC_ALL, "rus");
 
-void StartGame(int first_value, int last_value)
-{
-	int random_number = first_value + rand() % last_value;
-	cout << "Okay. Try to guess a number\n";
-	int num = random_number-1;
-	while (num != random_number)
-	{
-		num = TryFoundNumber(num);
-		if(num > random_number)
-		{
-			cout << "My number is lower\n";
-		}
-		else if(num < random_number)
-		{
-			cout << "My number is higher\n";
-		}
-	}
-	cout << "Yes! You win!\nWant play Again?(yes/no)\n";
-	char* str = new char[];
-	cin >> str;
-	if (strcmp(str, "yes") == 0)
-	{
-		InitMindGame();
-	}
-	else if(strcmp(str, "no") == 0)
-	{
-		cout<<"\nEnter command:\nRun\nExit\n\n";
-		InitGame();	
-	}
-	else
-	{
-		cout<<"Invalid command. Returning to Main Menu.";
-		cout<<"\nEnter command:\nRun\nExit\n\n";
-		InitGame();	
-	}
-}
-
-void InitGame()
-	{
-		char* s = new char[];
-		cin >> s;
-		if (strcmp(s, "Exit") == 0)
-		{
-			return;
-		}
-		else if(strcmp(s, "Run") == 0)
-		{
-			InitMindGame();
-		}
-		else
-		{
-			cout<<"Wrong Command. Try else.\n";
-			cout<<"Enter command:\nRun\nExit\n\n";
-			InitGame();	
-		}
-	}
-
-int main()
-{	
-	cout<<"Enter command:\nRun\nExit\n\n";	
-	
-	InitGame();	
-	system("pause");
+	int count = 200;
+	printf("Число вариантов размена равно %d\n", N(count, 1000));
+	system ("pause");
 	return 0;
 }
